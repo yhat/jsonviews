16f11ef9dc9cd1c600eacba04bb1cec5d58ae02d
chiefsumbrero <dev@yhathq.com>
2015-01-30 11:25:18 -0500

    more rigorous tests added

--- jsonviews_test.go
+++ jsonviews_test.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
-	"os"
+	"io/ioutil"
 	"strings"
 	"testing"
 	"unicode/utf8"
@@ -145,8 +146,7 @@ func TestReadObject(t *testing.T) {
 	view.AddFilter(".menu.popup")
 	view.AddFilter(".menu.id")
 	view.AddFilter(".menu.tf")
-	r := io.TeeReader(view, os.Stderr)
-	d := json.NewDecoder(r)
+	d := json.NewDecoder(view)
 	s := struct {
 		Menu struct {
 			Id    string `json:"id"`
@@ -171,3 +171,182 @@ func TestReadObject(t *testing.T) {
 		t.Errorf(".menu.id corrupted")
 	}
 }
+
+type ViewTest struct {
+	Input   string
+	Filters []string
+	Output  string
+	OK      bool
+}
+
+func (vt ViewTest) Run() error {
+	v := NewView(strings.NewReader(vt.Input))
+	for _, f := range vt.Filters {
+		v.AddFilter(f)
+	}
+	out, err := ioutil.ReadAll(v)
+	if err != nil {
+		if vt.OK {
+			return fmt.Errorf("test returned error: %v", err)
+		}
+		return nil
+	}
+	if !vt.OK {
+		return fmt.Errorf("expected test to fail")
+	}
+	output := string(out)
+	if output != vt.Output {
+		return fmt.Errorf("expected '%s' got '%s'", vt.Output, output)
+	}
+	return nil
+}
+
+func TestViews(t *testing.T) {
+	for _, vt := range ViewTests {
+		if err := vt.Run(); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+var ViewTests = []ViewTest{
+	ViewTest{
+		Input:   Example1,
+		Filters: []string{".glossary.title"},
+		Output:  `{"glossary":{"title":"example glossary"}}`,
+		OK:      true,
+	},
+	ViewTest{
+		Input:   Example1,
+		Filters: []string{".glossary.title", ".glossary.GlossDiv.title"},
+		Output:  `{"glossary":{"title":"example glossary","GlossDiv":{"title":"S"}}}`,
+		OK:      true,
+	},
+	ViewTest{
+		Input: Example1,
+		Filters: []string{
+			".glossary.GlossDiv.GlossList.GlossEntry.ID",
+			".glossary.GlossDiv.GlossList.GlossEntry.Abbrev",
+		},
+		Output: `{"glossary":{"GlossDiv":{"GlossList":{"GlossEntry":{"ID":"SGML","Abbrev":"ISO 8879:1986"}}}}}`,
+		OK:     true,
+	},
+	ViewTest{
+		Input: Example1,
+		Filters: []string{
+			".glossary.GlossDiv.GlossList.GlossEntry.ID",
+			".glossary.GlossDiv.GlossList.GlossEntry.Abbrev",
+			".glossary.GlossDiv.GlossList.UnknownEntry.Abbrev",
+		},
+		Output: `{"glossary":{"GlossDiv":{"GlossList":{"GlossEntry":{"ID":"SGML","Abbrev":"ISO 8879:1986"}}}}}`,
+		OK:     true,
+	},
+	ViewTest{
+		Input:   Example1,
+		Filters: []string{},
+		Output:  `{}`,
+		OK:      true,
+	},
+	ViewTest{
+		Input: Example1,
+		Filters: []string{
+			".glossary.GlossDiv.GlossList.GlossEntry.ID",
+			".glossary.GlossDiv.GlossList.GlossEntry.Abbrev",
+			".glossary.GlossDiv.GlossList.GlossEntry.GlossDef",
+		},
+		Output: `{"glossary":{"GlossDiv":{"GlossList":{"GlossEntry":{"ID":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso":["GML","XML"]}}}}}}`,
+		OK:     true,
+	},
+}
+
+// Examples take from http://json.org/example
+var (
+	Example1 = `
+{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+`
+	Example2 = `{"menu": {
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
+  }
+}}`
+	Example3 = `{"widget": {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": { 
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}}`
+	Example5 = `{"menu": {
+    "header": "SVG Viewer",
+    "items": [
+        {"id": "Open"},
+        {"id": "OpenNew", "label": "Open New"},
+        null,
+        {"id": "ZoomIn", "label": "Zoom In"},
+        {"id": "ZoomOut", "label": "Zoom Out"},
+        {"id": "OriginalView", "label": "Original View"},
+        null,
+        {"id": "Quality"},
+        {"id": "Pause"},
+        {"id": "Mute"},
+        null,
+        {"id": "Find", "label": "Find..."},
+        {"id": "FindAgain", "label": "Find Again"},
+        {"id": "Copy"},
+        {"id": "CopyAgain", "label": "Copy Again"},
+        {"id": "CopySVG", "label": "Copy SVG"},
+        {"id": "ViewSVG", "label": "View SVG"},
+        {"id": "ViewSource", "label": "View Source"},
+        {"id": "SaveAs", "label": "Save As"},
+        null,
+        {"id": "Help"},
+        {"id": "About", "label": "About Adobe CVG Viewer..."}
+    ]
+}}`
+)
